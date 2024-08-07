ea5595fca59e1f74c4270433edde1a9ff748b348
Almog Gavra <almog.gavra@gmail.com>
2024-08-07 15:26:25 -0700

    opengraph image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -53,7 +53,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/slatedb-opengraph.jpg',
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
